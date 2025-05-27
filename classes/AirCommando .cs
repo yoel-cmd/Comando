9c6f8f86f7e75bb5b3cd1125b114b9cc592726c7
yoel <yoeli5746@gmail.com>
2025-05-27 15:59:46 +0300

    aI added an enemy unit and created a formation of soldiers.

---
+++ classes/AirCommando .cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class AirCommando : Commando
+{
+    public string typeComander { get; set; } = "Can fall";
+
+    //----------------------------------------------------------
+    public AirCommando(string name, string nameKode, string status) :base( name, nameKode, status) 
+    {
+        
+    }
+    //----------------------------------------------------------
+    public void print()
+    {
+        Console.WriteLine(typeComander);
+    }
+    //----------------------------------------------------------
+    public override void Attack()
+    {
+        Console.WriteLine($"The soldier{NameKode} from AirCommand  is Attack");
+    }
+    //----------------------------------------------------------
+
+
+}
---
+++ classes/Enemy.cs
@@ -0,0 +1,15 @@
+﻿public class Enemy
+{
+    public string name { get; set; }
+    public int life { get; set; } = 100;
+    public bool status { get; set; }
+    public string shout { get; } = "I am an enemy.";
+    //----------------------------------------------------------
+    public Enemy(string name, bool status)
+    {
+        this.name = name;
+        this.status = status;
+    }
+    //----------------------------------------------------------
+
+}
